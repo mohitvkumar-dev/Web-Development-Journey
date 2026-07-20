de21ae27be29f58a65c2265a6ce0e97244e14513
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-20 09:11:36 +0530

    objects of objects , array of objects

---
+++ JAVASCRIPT/DAY4/04_objects_of_objects.js
@@ -0,0 +1,12 @@
+const classInfo={
+    aman :{
+      grade:"A+",
+      city: "Delhi"
+
+    },
+
+    mohit :{
+       garde : "A++",
+       city: "Bettiah"
+    }
+};
---
+++ JAVASCRIPT/DAY4/05_array_of_objects.js
@@ -0,0 +1,11 @@
+const classInfo=[
+    { name: "Aman",
+      grade:"A+",
+      city: "Delhi"
+
+    },
+    {  name : "Mohit",
+       garde : "A++",
+       city: "Bettiah"
+    }
+];
