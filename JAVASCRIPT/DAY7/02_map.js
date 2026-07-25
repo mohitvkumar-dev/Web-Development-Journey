da64ab5df965dac3ddc8f2e30634a969137fd8eb
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-25 22:12:36 +0530

    map

---
+++ JAVASCRIPT/DAY7/02_map.js
@@ -0,0 +1,24 @@
+
+let arr=[1,2,3];
+let double = arr.map(function(el){
+    return el*2;
+});
+
+
+
+
+
+
+let students=[{
+    name: "Mohit",
+    marks: 97,
+},
+{
+    name: "Aman",
+    marks:90,
+}];
+
+
+let cgpa = students.map(function(el){
+    return el.marks/10;
+});
