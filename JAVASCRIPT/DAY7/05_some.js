a3b5dc3b3f7b9cfd39c9bf11c1da5c58c9f3f09d
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-26 20:49:48 +0530

    every method

---
+++ JAVASCRIPT/DAY7/05_some.js
@@ -0,0 +1,4 @@
+[1,2,34,5].some((el)=>(el%2==0));
+[2,34].some((el)=>(el%2==0));
+
+// write in console to see the result
---
+++ JAVASCRIPT/DAY7/06_reduce.js
@@ -0,0 +1,3 @@
+// write in console
+
+[1,2,3,5,6].reduce((res,el)=>(res+el));
