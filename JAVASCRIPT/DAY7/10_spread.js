55c14fc0219461df006155bf2e474cb5a6601276
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-27 13:39:19 +0530

    spread

---
+++ JAVASCRIPT/DAY7/10_spread.js
@@ -0,0 +1,9 @@
+
+
+// it is used to see elelments in spread form means alg alg
+
+ console.log(..."IAS MOHIT KUMAR");
+
+ let arr=[1,23,3,4];
+ console.log(Math.min(...arr));
+ console.log(...arr);
