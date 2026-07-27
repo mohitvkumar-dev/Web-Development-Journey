4b83bbc097fa57050eecef8fd99ce7929d53e5e9
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-27 13:04:50 +0530

    reduce

--- JAVASCRIPT/DAY7/06_reduce.js
+++ JAVASCRIPT/DAY7/06_reduce.js
@@ -1,3 +1,15 @@
 // write in console
 
-[1,2,3,5,6].reduce((res,el)=>(res+el));
+// [1,2,3,5,6].reduce((res,el)=>(res+el));
+
+let nums=[2,4,5,3,6];
+
+let max= nums.reduce((max,el) =>{
+    if(max<el){
+        return el;
+    }else{
+        return max;
+    }
+});
+
+console.log(max);
