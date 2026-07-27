dbfe4d781b82320f1f2cea83eec1ce3a2fa4b0d4
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-27 15:25:29 +0530

    assignment

---
+++ JAVASCRIPT/DAY7/16_assignment.js
@@ -0,0 +1,20 @@
+let nums=[1,2,3,4,5];
+let sqr=nums.map((num)=> num*num);
+console.log(sqr);
+
+
+let sum = sqr.reduce((acc,cur)=> acc+cur,0);
+let avg = sum/nums.length;
+console.log(avg);
+
+
+let arr=[2,4,5,6,7];
+console.log(arr.map((el)=> el+5));
+
+
+let str=["adam","mohit","rahul"];
+console.log(str.map((char)=>char.toUpperCase()));
+
+const mergObj=(obj1,obj2)=>({...obj1,obj2});
+mergObj({a:1,b:2},{c:3,d:4});
+
