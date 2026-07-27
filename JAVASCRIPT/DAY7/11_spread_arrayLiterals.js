7e4143c33c39fdc0f67dcec0c384325931f43f20
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-27 13:51:22 +0530

    spread with array literals

---
+++ JAVASCRIPT/DAY7/11_spread_arrayLiterals.js
@@ -0,0 +1,12 @@
+let arr =[1,2,3,4,5,56];
+let newArr=[...arr];
+console.log(newArr);
+console.log(arr);
+
+let char=['m','o','h','i','t'];
+
+let newChar=[...char];
+console.log(char);
+
+let val=[...newArr,...newChar];
+console.log(val);
