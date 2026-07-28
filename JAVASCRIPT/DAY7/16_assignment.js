3f8e816a9fa61581f2a079c4926fa9fe6c9d1b61
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-28 21:17:49 +0530

    assignment

--- JAVASCRIPT/DAY7/16_assignment.js
+++ JAVASCRIPT/DAY7/16_assignment.js
@@ -1,3 +1,5 @@
+
+
 let nums=[1,2,3,4,5];
 let sqr=nums.map((num)=> num*num);
 console.log(sqr);
@@ -11,10 +13,11 @@ console.log(avg);
 let arr=[2,4,5,6,7];
 console.log(arr.map((el)=> el+5));
 
-
 let str=["adam","mohit","rahul"];
 console.log(str.map((char)=>char.toUpperCase()));
 
+
+// question no:5
 const mergObj=(obj1,obj2)=>({...obj1,obj2});
 mergObj({a:1,b:2},{c:3,d:4});
 
