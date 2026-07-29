34ee3c0c08e52781d251c83f554e14e754d10c3e
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-29 11:41:49 +0530

    getElementById

---
+++ JAVASCRIPT/DAY8/01_DOM.js
@@ -0,0 +1,8 @@
+let object= document.getElementById("mainImg");
+console.dir(object); // to get the elements
+console.dir(object.tagName); // print tagName
+console.dir(object.id); // print id
+console.dir(object.src) // print object source
+
+//  to update 
+console.dir(object.src="https://picsum.photos/200/250?1");
