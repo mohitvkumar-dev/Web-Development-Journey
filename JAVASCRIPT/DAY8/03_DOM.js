24c91c989d1626974d8234ad180cd8170dcf3f8a
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-29 12:52:51 +0530

    getElementsByTagName

---
+++ JAVASCRIPT/DAY8/03_DOM.js
@@ -0,0 +1,13 @@
+
+// to access HTML collection
+console.dir(document.getElementsByTagName("p"));
+
+// to access individual
+console.dir(document.getElementsByTagName("p")[0]);
+console.dir(document.getElementsByTagName("p")[1]);
+console.dir(document.getElementsByTagName("p")[2]);
+
+
+//  to update
+
+console.dir(document.getElementsByTagName("p")[0].innerText="IAS MOHIT KUMAR");
