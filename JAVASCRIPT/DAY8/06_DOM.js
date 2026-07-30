d3a9425e9561a3e9a29d98b0e164bde8d98f3e83
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-30 15:00:46 +0530

    Manipulating Attributes

---
+++ JAVASCRIPT/DAY8/06_DOM.js
@@ -0,0 +1,6 @@
+let img=document.querySelector('img');
+console.dir(img);
+console.dir(img.getAttribute('id'));
+console.dir(img.setAttribute('id','spidermanImg'));
+console.dir(img.src);
+console.dir(img.setAttribute('img.src', 'https://picsum.photos/200/250?3'));
