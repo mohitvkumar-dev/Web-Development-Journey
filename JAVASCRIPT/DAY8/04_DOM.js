a6ddc8ca3e74e7f2565110baeb32e20ea40657e4
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-30 10:21:15 +0530

    Query Selectors

---
+++ JAVASCRIPT/DAY8/04_DOM.js
@@ -0,0 +1,5 @@
+console.dir(document.querySelector("h1"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".heroImg"));
+console.dir(document.querySelectorAll(".heroImg"));
+console.dir(document.querySelector("section a")); // gives section first's anchor tag
