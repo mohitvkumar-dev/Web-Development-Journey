08b4c4d331fda2ef1f05922e5caaeb7c452378ad
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-31 11:41:08 +0530

    Navigation

---
+++ JAVASCRIPT/DAY8/08_DOM.js
@@ -0,0 +1,4 @@
+let img = document.querySelector('img');
+console.dir(img.classList);
+let abc =img.classList.add("abc");
+console.dir(abc.classList);
---
+++ JAVASCRIPT/DAY8/09_DOM.js
@@ -0,0 +1,13 @@
+// parentElement
+let h3=document.querySelector('h3');
+console.dir(h3.parentElement);
+
+// children
+let section=document.querySelector('section')
+console.dir(section.children);
+
+// previousElementSibling/nextElementSibling
+
+let div= document.querySelector('div');
+console.dir(div.previousElementSibling);
+console.dir(div.nextElementSibling);
