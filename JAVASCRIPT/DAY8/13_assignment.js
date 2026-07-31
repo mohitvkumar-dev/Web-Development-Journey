8a169c7226afaddd8ffe10f0d446cc0e756be85a
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-31 13:49:03 +0530

    assignment

---
+++ JAVASCRIPT/DAY8/13_assignment.js
@@ -0,0 +1,22 @@
+let button = document.createElement("button");
+let input = document.createElement("input");
+button.innerText = "Click me";
+document.querySelector("body").append(input);
+document.querySelector("body").append (button)
+
+button.setAttribute("id", "btn");
+input.setAttribute("placeholder", "username");
+
+
+let btn = document.querySelector("#btn");
+btn.classList.add("btnStyle");
+
+
+let h1 = document.createElement("h1");
+hl.innerHTML = "<u>DOM Practice</u>";
+document.querySelector("body").append(hl);
+
+
+let p = document.createElement("p") ;
+p.innerHTML = "Apna College <b>Delta</b> Practice";
+document.querySelector("body").append(p);
