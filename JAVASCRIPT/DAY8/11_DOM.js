56e35e6e1e8b90350c8ae6882cc475b8d4f3f5bc
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-07-31 13:39:32 +0530

    Practice

--- JAVASCRIPT/DAY8/11_DOM.js
+++ JAVASCRIPT/DAY8/11_DOM.js
@@ -3,9 +3,9 @@ console.dir(btn.innerHTML="New button");
 let p= document.querySelector('p');
 console.dir(p.insertAdjacentElement('beforebegin',btn));
 
-
+// removeChild()
 let header=document.querySelector('header');
 console.dir(header.removeChild(btn));
 
-
+// remove()
 console.dir(header.remove());
---
+++ JAVASCRIPT/DAY8/12_prac.js
@@ -0,0 +1,15 @@
+let para1= document.createElement('p');
+para1.innerText="I'm new para";
+document.querySelector('body').append(para1);
+
+
+para1.classList.add("red");
+
+
+let h3= document.createElement('h3');
+h3.innerText="I'm blue";
+document.querySelector('body').append(h3);
+
+
+h3.classList.add("blue");
+
