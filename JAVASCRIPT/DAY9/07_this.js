bfc78302f25d8984d53bf62f669d98932a4172c8
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-08-07 12:14:32 +0530

    this  in Event Listeners

---
+++ JAVASCRIPT/DAY9/07_this.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector("button");
+let p = document.querySelector("p");
+let h5 = document.querySelector("h5");
+
+
+function changeColor(){
+   console.dir(this.innerText);
+   this.style.backgroundColor = "blue";
+}
+
+btn.addEventListener("click", changeColor);
+p.addEventListener("click", changeColor);
+h5.addEventListener("click", changeColor);
+
+
