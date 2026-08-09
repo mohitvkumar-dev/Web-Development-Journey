451b72e177e36938bc9688993d7caaf535e93541
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-08-09 18:28:22 +0530

    DAY9 Completed

--- JAVASCRIPT/DAY9/12_inputEvent.js
+++ JAVASCRIPT/DAY9/12_inputEvent.js
@@ -5,12 +5,14 @@ form.addEventListener("submit", function(event){
 });
 
 
+// 
 let user= document.querySelector("#user");
 
 user.addEventListener("change",function(){
     console.log("change event");
     console.log("final value =", this.value);
 });
+
 
 user.addEventListener("input",function(){
     console.log("input event");
