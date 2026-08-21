abb78bd088af2bf70cf6512dd43df9982b80abcb
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-08-21 17:10:31 +0530

    Event Bubbling

---
+++ JAVASCRIPT/DAY10/01_Event_Bubbling.js
@@ -0,0 +1,23 @@
+let div= document.querySelector("div");
+let ul= document.querySelector("ul");
+let li= document.querySelector("li");
+
+div.addEventListener("click",function(){
+    console.log("div was clicked");
+});
+
+ul.addEventListener("click",function(){
+    console.log("ul was clicked");
+});
+
+li.addEventListener("click",function(){
+    console.log("li was clicked");
+});
+
+
+for(li of lis){
+    
+li.addEventListener("click",function(){
+    console.log("li was clicked");
+});
+}
