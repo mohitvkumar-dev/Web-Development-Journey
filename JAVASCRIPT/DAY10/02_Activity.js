63eca0f9c5a9b70520baa2b597bb651331b764cd
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-08-22 17:35:09 +0530

    Event Delegatation

---
+++ JAVASCRIPT/DAY10/02_Activity.js
@@ -0,0 +1,41 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+
+btn.addEventListener("click",function(){
+    let item= document.createElement("li");
+    item.innerText = inp.value;
+
+     let delbtn= document.createElement("button");
+     delbtn.innerText ="x";
+     delbtn.classList.add("delete");
+
+     item.appendChild(delbtn);
+
+    ul.appendChild(item);
+    inp.value=""; //  print hone ke bad apne app khali ho jayega
+
+});
+
+//   Event Delegatations
+
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listitem= event.target.parentElement;
+        listitem.remove();
+       console.log("deleted");
+    }
+    
+});
+
+
+
+// let delbtns= document.querySelectorAll(".delete");
+// for(delbtn of delbtns){
+//     delbtn.addEventListener("click", function(){
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
