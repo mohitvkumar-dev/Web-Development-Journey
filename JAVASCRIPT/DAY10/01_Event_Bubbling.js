d94376dce3a481bde960ce538341f3bf8ad8c03b
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-08-22 10:19:40 +0530

    Event Building

--- JAVASCRIPT/DAY10/01_Event_Bubbling.js
+++ JAVASCRIPT/DAY10/01_Event_Bubbling.js
@@ -10,14 +10,16 @@ ul.addEventListener("click",function(){
     console.log("ul was clicked");
 });
 
-li.addEventListener("click",function(){
+li.addEventListener("click",function(event){
+    event.stopPropagation();
     console.log("li was clicked");
 });
 
 
 for(li of lis){
     
-li.addEventListener("click",function(){
+li.addEventListener("click",function(event){
+    event.stopPropagation();
     console.log("li was clicked");
-});
+}); 
 }
