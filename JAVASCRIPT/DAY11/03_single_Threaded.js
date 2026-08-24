92371177676ee282a1c79fa8ceaaa404c7d23dc6
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-08-24 22:17:35 +0530

    JS as single Threaded

---
+++ JAVASCRIPT/DAY11/03_single_Threaded.js
@@ -0,0 +1,12 @@
+// let a=25;
+// console.log(a);
+
+// let b=56;
+// console.log(b);
+
+// console.log(a+b);
+
+setTimeout(function(){
+    console.log("Mohit Kumar Paswan");
+},2000);
+console.log("hello");
