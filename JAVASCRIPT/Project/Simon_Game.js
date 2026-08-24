bf47d24def95373343c8a7b7f53ea1dd99484075
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-08-24 17:15:52 +0530

    Add Simon Game Project

--- JAVASCRIPT/Project/Simon_Game.js
+++ JAVASCRIPT/Project/Simon_Game.js
@@ -34,6 +34,7 @@ function userFlash(btn){
 }
 
 function levelup(){
+    userSequence=[];
   level++;
   h2.innerText= `Level ${level}`; 
   
@@ -41,19 +42,52 @@ function levelup(){
   let randomIdx=Math.floor(Math.random()*btns.length);
   let randomColor= btns[randomIdx];
   let randombtn = document.querySelector( `.${randomColor}`);
-  console.log(randomIdx);
-  console.log(randomColor);
-  console.log(randombtn);
-  gameFlash(randombtn);
+
+     gameSequence.push(randomColor);
+     console.log(gameSequence);
+     gameFlash(randombtn);
+}
+
+
+function checkAns(idx){
+
+
+if(userSequence[idx]===gameSequence[idx]){
+    if(userSequence.length== gameSequence.length){
+        setTimeout(levelup,1000);
+    }
+    
+}else{
+     h2.innerHTML= `Game Over! Your score is <b>${level}</b> <br> Press any key to start.`; 
+     document.querySelector("body").style.backgroundColor = "red";
+     setTimeout(function(){
+       document.querySelector("body").style.backgroundColor = "white";
+     }, 150);
+     reset();
+}
 }
 
 
 function btnPress(){
+  
     let btn = this;
     userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSequence.push(userColor);
+    checkAns(userSequence.length-1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for(btn of allBtns){
     btn.addEventListener("click",btnPress);
+}
+ 
+
+function reset(){
+    started= false;
+    gameSequence=[];
+    userSequence=[];
+    level=0;
+
 }
