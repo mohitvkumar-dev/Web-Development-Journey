c59cc3ae5df2538f6540bc8f32556e041526d756
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-08-27 18:35:26 +0530

    API req using fetch()

---
+++ JAVASCRIPT/DAY12/05_API_request.js
@@ -0,0 +1,13 @@
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+.then((response)=>{
+    console.log(response);
+    response.json().then((data)=>{
+        console.log(data);
+    });
+
+})
+.catch((err)=>{
+    console.log("ERROR - ", err);
+})
