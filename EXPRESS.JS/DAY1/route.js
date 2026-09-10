aece0b856572b4716d0710c5705a0bf2c2b5aab8
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-09-10 17:01:02 +0530

    Routing

---
+++ EXPRESS.JS/DAY1/route.js
@@ -0,0 +1,40 @@
+// Listening Requests
+
+const express = require('express');
+const app = express();
+
+
+let port= 3000;
+
+app.listen(port, ()=>{
+    console.log(`app is listeninig on port ${port}`);
+});
+
+
+// Routing
+
+app.get("/", (req,res)=>{
+    res.send("You contacted root path");
+
+});
+
+app.get("/search", (req,res)=>{
+    res.send("You contacted search path");
+
+});
+
+app.get("/home", (req,res)=>{
+    res.send("You contacted home path");
+
+});
+app.get("/{*splat}", (req,res)=>{  // Wildcard response
+    res.send("This path doesn't exist");
+
+});
+
+
+
+app.post("/", (req,res)=>{  // Wildcard response
+    res.send("You send a post request to root");
+
+});
