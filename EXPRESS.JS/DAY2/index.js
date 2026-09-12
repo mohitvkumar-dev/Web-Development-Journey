fc9c8dc53677d43faae7674240e46087cbba67bd
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-09-12 16:30:21 +0530

    Instagram Activity

--- EXPRESS.JS/DAY2/index.js
+++ EXPRESS.JS/DAY2/index.js
@@ -11,6 +11,11 @@ app.set("views", path.join(__dirname,"/views"));
 app.get("/", (req, res)=>{
     res.render("home");
 });
+
+app.get("/ig/:username", (req, res)=>{
+   let {username }= req.params;
+   res.render("instagram.ejs", {username});
+});
 app.get("/rolldice", (req, res)=>{
     let diceVal = Math.floor(Math.random() *6)+1;
     res.render("rolldice.ejs", {diceVal});
