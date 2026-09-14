5118f3f767d8ff5d22bd30de70dc3f5eed21c202
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-09-14 17:50:47 +0530

    Instragram Template

--- EXPRESS.JS/DAY2/index.js
+++ EXPRESS.JS/DAY2/index.js
@@ -13,10 +13,16 @@ app.get("/", (req, res)=>{
 });
 
 app.get("/ig/:username", (req, res)=>{
-
-    const followers=["Mohit", "Paswan","Abhishek", "Rupak", "Aman" ];
-   let {username }= req.params;
-   res.render("instagram.ejs", {username,followers});
+   let {username} = req.params;
+   const instaData = require("./data.json");
+   const data = instaData[username];
+   if(data){
+     res.render("instagram.ejs",{data });
+   }else{
+    res.render("error.ejs");
+   }
+  
+  
 });
 app.get("/rolldice", (req, res)=>{
     let diceVal = Math.floor(Math.random() *6)+1;
