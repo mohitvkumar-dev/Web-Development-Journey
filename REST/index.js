71c3b84b4c99c8f1e8e9723da9da754bb245a19c
mohitvkumar-dev <mohitvkumar.in@gmail.com>
2026-09-21 17:55:46 +0530

    Implement POST and Redirect

--- REST/index.js
+++ REST/index.js
@@ -30,8 +30,18 @@ let posts =[
 
 app.get("/posts", (req,res)=>{
     res.render("index.ejs",{posts});
-})
+});
 
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    let {username,content} = req.body;
+    posts.push({username,content});
+    res.redirect("/posts");
+});
 app.listen(port,()=>{
     console.log("Listening to port");
 });
